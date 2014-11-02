c57f681991eb25d8921f96dfb3d80c407cce1b15
marutaru <uskmsy@sfc.keio.ac.jp>
2014-11-02 19:30:54 +0900

    fix label change

--- js/note.coffee
+++ js/note.coffee
@@ -6,41 +6,33 @@ $ ->
   $(document).ready () ->
     socket.emit "find notes",location.href
     chrome.storage.local.get (val) ->
-      console.log val["label"]
       label = val["label"]
 
   # note
   $("body").dblclick (e) ->
     console.log e
-    note =
-      time:e.timeStamp
-      screenX:e.screenX
-      screenY:e.screenY
-      pageX:e.pageX
-      pageY:e.pageY
-      clientX:e.clientX
-      clientY:e.clientY
-      offsetX:e.offsetX
-      offsetY:e.offsetY
-      uri:e.target.baseURI
-      label:label
-      id:e.target.id if e.target.id
-    if e.which is 1
+    chrome.storage.local.get (val) ->
+      console.log val["label"]
+      label = val["label"]
+      note =
+        time:e.timeStamp
+        screenX:e.screenX
+        screenY:e.screenY
+        pageX:e.pageX
+        pageY:e.pageY
+        clientX:e.clientX
+        clientY:e.clientY
+        offsetX:e.offsetX
+        offsetY:e.offsetY
+        uri:e.target.baseURI
+        label:label
+        id:e.target.id if e.target.id
       console.log "left dblclick"
       $("body").append "<span id='webit-note' class='label label-#{label}'>#{label}</span>"
       .children(":last").css "position","absolute"
       .css "left","#{e.pageX}px"
       .css "top","#{e.pageY}px"
       socket.emit "send note",note
-    ###
-    else if e.which is 3
-      console.log "right dblclick"
-      $("body").append "<span id='webit-note' class='label label-danger'>info</span>"
-      .children(":last").css "position","absolute"
-      .css "left","#{e.pageX}px"
-      .css "top","#{e.pageY}px"
-      socket.emit "send note",note
-    ###
 
   # receive notes
   socket.on "send notes",(notes)->
--- js/note.js
+++ js/note.js
@@ -7,42 +7,33 @@
     $(document).ready(function() {
       socket.emit("find notes", location.href);
       return chrome.storage.local.get(function(val) {
-        console.log(val["label"]);
         return label = val["label"];
       });
     });
     $("body").dblclick(function(e) {
-      var note;
       console.log(e);
-      note = {
-        time: e.timeStamp,
-        screenX: e.screenX,
-        screenY: e.screenY,
-        pageX: e.pageX,
-        pageY: e.pageY,
-        clientX: e.clientX,
-        clientY: e.clientY,
-        offsetX: e.offsetX,
-        offsetY: e.offsetY,
-        uri: e.target.baseURI,
-        label: label,
-        id: e.target.id ? e.target.id : void 0
-      };
-      if (e.which === 1) {
+      return chrome.storage.local.get(function(val) {
+        var note;
+        console.log(val["label"]);
+        label = val["label"];
+        note = {
+          time: e.timeStamp,
+          screenX: e.screenX,
+          screenY: e.screenY,
+          pageX: e.pageX,
+          pageY: e.pageY,
+          clientX: e.clientX,
+          clientY: e.clientY,
+          offsetX: e.offsetX,
+          offsetY: e.offsetY,
+          uri: e.target.baseURI,
+          label: label,
+          id: e.target.id ? e.target.id : void 0
+        };
         console.log("left dblclick");
         $("body").append("<span id='webit-note' class='label label-" + label + "'>" + label + "</span>").children(":last").css("position", "absolute").css("left", "" + e.pageX + "px").css("top", "" + e.pageY + "px");
         return socket.emit("send note", note);
-      }
-
-      /*
-      else if e.which is 3
-        console.log "right dblclick"
-        $("body").append "<span id='webit-note' class='label label-danger'>info</span>"
-        .children(":last").css "position","absolute"
-        .css "left","#{e.pageX}px"
-        .css "top","#{e.pageY}px"
-        socket.emit "send note",note
-       */
+      });
     });
     return socket.on("send notes", function(notes) {
       var value, _i, _len, _results;
